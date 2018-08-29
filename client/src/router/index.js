eb4f4eb1d15b2a66e7d72c69719b6e3f30ffd748
StrndedLeander <strnded.vandiepen@gmail.com>
2018-08-29 21:58:02 +0200

    router

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "@/components/Home";
+import Bets from "@/components/Bets";
 
 Vue.use(Router);
 
@@ -9,8 +9,8 @@ let router = new Router({
   routes: [
     {
       path: "/",
-      name: "Home",
-      component: Home
+      name: "Bets",
+      component: Bets
     },
     {
       path: "/register",
